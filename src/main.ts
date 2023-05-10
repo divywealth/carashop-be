359be31bdb64fb316ebc4598aea1d93ac33bb7e7
Divine <divywealth1@gmail.com>
2023-05-10 18:04:31 +0100

    CORS POLICY

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,18 @@ import { ConfigService } from '@nestjs/config';
 import { VersioningType } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors({
+  const app = await NestFactory.create(
+    AppModule, 
+    //{ cors: true }
+    );
+  /**app.enableCors({
     origin: 'http://localhost:8080',
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
     
   });
   app.enableVersioning({
     type: VersioningType.URI,
-  });
+  });**/
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
