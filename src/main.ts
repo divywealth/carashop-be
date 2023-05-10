e58b7378197fb373038ebc71f346dab6037a9123
Divine <divywealth1@gmail.com>
2023-05-10 18:32:08 +0100

    CORS POLICY

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:8080'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   });
   app.enableVersioning({
