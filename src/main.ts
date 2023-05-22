4c129e2d7cea237b8f2083972e56219b1c2b7f15
Divine <divywealth1@gmail.com>
2023-05-22 12:23:15 +0100

    expire date

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     { cors: true }
     );
   app.enableCors({
-    origin: ['http://localhost:8080', 'https://cara-shop.herokuapp.com'],
+    origin: ['http://localhost:8080', 'http://cara-shop.herokuapp.com'],
     methods: ['POST', 'PUT', 'DELETE', 'GET'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
