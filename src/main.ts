560720ca9f36404af06b26163c4180a0784d15ac
Divine <divywealth1@gmail.com>
2023-07-23 06:45:17 +0100

    Http CORS

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:8080',
-      'http://54.173.189.226/',
+      'http://54.173.189.226',
       'http://cara-shop.herokuapp.com',
       'https://cara-shop.herokuapp.com',
     ],
